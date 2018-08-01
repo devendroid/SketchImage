42288fa965c553e76d049daddbad9f4535b1dbfa
devsideal <devsp321@gmail.com>
2018-08-01 18:26:02 +0530

    Fixed OutOfMemoryError in demo

--- app/src/main/java/com/devs/sketchimagedemo/MainActivity.java
+++ app/src/main/java/com/devs/sketchimagedemo/MainActivity.java
@@ -1,9 +1,11 @@
 package com.devs.sketchimagedemo;
 
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -11,7 +13,6 @@ import android.widget.ImageView;
 import com.devs.imagetosketch.R;
 import com.devs.sketchimage.SketchImage;
 
-// https://github.com/yaa110/Effects-Pro?utm_source=android-arsenal.com&utm_medium=referral&utm_campaign=1299
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -30,8 +31,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ((Button)findViewById(R.id.btn_gray)).setOnClickListener(this);
         ((Button)findViewById(R.id.btn_invert)).setOnClickListener(this);
 
-        Bitmap bmOriginal = BitmapFactory.decodeResource(getResources(), R.drawable.usr);
+
+        //Bitmap bmOriginal = BitmapFactory.decodeResource(getResources(), R.drawable.usr);
+        Bitmap bmOriginal = decodeSampledBitmapFromResource(getResources(), R.drawable.hd_img, 100, 100);
+
         target = (ImageView) findViewById(R.id.iv_targe);
+
+        target.setImageBitmap(bmOriginal);
 
         sketchImage = new SketchImage.Builder(this, bmOriginal).build();
     }
@@ -55,6 +61,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 target.setImageBitmap(sketchImage.getImageAs(SketchImage.INVERT));
                 break;
         }
+    }
+
+
+    /*
+    For More Detail:
+    https://developer.android.com/topic/performance/graphics/load-bitmap
+    */
+    private Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                   int reqWidth, int reqHeight) {
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    private int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
     }
 
 }
